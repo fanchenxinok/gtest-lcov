b80dc3b054391a40ed8d881a134292b1a302ac51
fanchenxin <Chenxin.Fan@sitrigroup.com>
2022-06-20 16:31:13 +0800

    删除不需要关心的头文件覆盖率信息

--- gtest_lcov/run.sh
+++ gtest_lcov/run.sh
@@ -12,6 +12,9 @@ lcov-1.11/bin/lcov -d ../ -t 'appMain' -o 'appMain.info' -b . -c
 #/home/fanchenxin/tools/lcov-1.11/bin/lcov -d ../ -t 'appMain' -o 'appMain.info' -b . -c
 #/home/fanchenxin/tools/lcov-1.11/bin/lcov --directory . --output-file test_case.info --test-name  test_case
 
+#去除不关心的文件覆盖率信息，比如头文件
+lcov-1.11/bin/lcov -r appMain.info '*.h' '/usr/include/*' -o appMain.info
+
 #../lcov-1.11/bin/genhtml -o . test_case.info
 #/home/fanchenxin/tools/lcov-1.11/bin/genhtml appMain.info --quiet --output-directory lcov_out --title "appMain"
 lcov-1.11/bin/genhtml appMain.info --quiet --output-directory lcov_out --title "appMain" 
