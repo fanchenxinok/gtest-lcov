5f37b6787d86d17d68f5f4ece6970228b3438d4b
fanchenxin <Chenxin.Fan@sitrigroup.com>
2022-06-20 15:08:20 +0800

    gtest_locv目录下locv-1.11目录变更

--- gtest_lcov/run.sh
+++ gtest_lcov/run.sh
@@ -8,13 +8,13 @@ make
 ./appMain
 
 ##  参数-d 后面跟的目录是.gcon .gcda 所在的根目录
-lcov -d ../ -t 'appMain' -o 'appMain.info' -b . -c
+lcov-1.11/bin/lcov -d ../ -t 'appMain' -o 'appMain.info' -b . -c
 #/home/fanchenxin/tools/lcov-1.11/bin/lcov -d ../ -t 'appMain' -o 'appMain.info' -b . -c
 #/home/fanchenxin/tools/lcov-1.11/bin/lcov --directory . --output-file test_case.info --test-name  test_case
 
 #../lcov-1.11/bin/genhtml -o . test_case.info
 #/home/fanchenxin/tools/lcov-1.11/bin/genhtml appMain.info --quiet --output-directory lcov_out --title "appMain"
-genhtml appMain.info --quiet --output-directory lcov_out --title "appMain" 
+lcov-1.11/bin/genhtml appMain.info --quiet --output-directory lcov_out --title "appMain" 
 
 firefox ./lcov_out/index.html
 
