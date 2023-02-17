cdc3fc37c2cca68570e40607d150a55a1df57704
fanchenxinok <531266381@qq.com>
2023-02-17 16:18:13 +0800

    gtest单元测试生成网页报告

--- gtest_lcov/run.sh
+++ gtest_lcov/run.sh
@@ -5,7 +5,7 @@ cd ./gtest_lcov
 make clean
 sudo rm -rf ./lcov_out/*
 make
-./appMain
+./appMain --gtest_output="xml:output.xml"
 
 ##  参数-d 后面跟的目录是.gcon .gcda 所在的根目录
 lcov-1.11/bin/lcov -d ../ -t 'appMain' -o 'appMain.info' -b . -c
@@ -18,6 +18,9 @@ lcov-1.11/bin/lcov -r appMain.info '*.h' '/usr/include/*' -o appMain.info
 #../lcov-1.11/bin/genhtml -o . test_case.info
 #/home/fanchenxin/tools/lcov-1.11/bin/genhtml appMain.info --quiet --output-directory lcov_out --title "appMain"
 lcov-1.11/bin/genhtml appMain.info --quiet --output-directory lcov_out --title "appMain" 
+
+#gtest test report
+xsltproc ./gtest2html/gtest2html.xslt output.xml > gtest_unittest.html
 
 firefox ./lcov_out/index.html
 
